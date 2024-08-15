d8a3e93922090d0a6e44b39870742188642c2bde
Dylan <dylanworth@protonmail.com>
2024-08-15 06:02:46 -0400

    Redesign of website

--- js/heart.js
+++
@@ -1,4 +0,0 @@
-function handleHeartClick(repository) {
-    // Here you can implement the logic to handle the heart button click for each repository
-    alert('You clicked the heart button for ' + repository);
-  }
--- test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk, PhotoImage, Label
-
-root = Tk()
-image = Image.open('font_bold.gif')  # Pillow supports many formats
-photo = ImageTk.PhotoImage(image)
-label = Label(root, image=photo)
-label.pack()
-root.mainloop()
